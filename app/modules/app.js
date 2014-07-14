0698b91942ce5d124396c7614862194edc07b435
Lucas Penney <lucaspenney@gmail.com>
2014-07-14 13:27:48 -0700

    Set up views, delete old copy code

--- app/modules/app.js
+++ app/modules/app.js
@@ -2,11 +2,19 @@ var app = angular.module('app', ['ngRoute', 'ngSanitize', 'ngAnimate', 'mgcrea.n
 	$locationProvider
 		.html5Mode(true)
 		.hashPrefix('!');
-	$routeProvider.when('/', {
+	$routeProvider.when('/about', {
 		templateUrl: 'app/views/home.html',
 		controller: homeController,
 	});
+	$routeProvider.when('/resume', {
+		templateUrl: 'app/views/resume.html',
+		controller: homeController,
+	});
+	$routeProvider.when('/work', {
+		templateUrl: 'app/views/work.html',
+		controller: homeController,
+	});
 	$routeProvider.otherwise({
-		redirectTo: '/'
+		redirectTo: '/about'
 	});
 });
